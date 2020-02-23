eb18e774a2b3c0935fb264288189e7205c9f489c
Maria Dlugosz <maria.a.dlugosz@gmail.com>
2020-02-23 22:07:19 +0100

    kwadrat sprawdzenie liczby dla obwodu

--- kwadrat.js
+++ kwadrat.js
@@ -1,9 +1,12 @@
 function obwod_kwadratu(bok) {
-    if(bok <= 0) {
-      document.write(bok + " musi być dodatni");
+    var wartosc_boku = parseFloat(bok);
+    if (isNaN(wartosc_boku)) {
+        document.write(bok + " nie jest liczbą.");
+    } else if(wartosc_boku <= 0) {
+      document.write(wartosc_boku + " musi być dodatni");
     } else { 
-      var obwod = 4 * bok;
-      document.write("Obwód kwadratu o boku " + bok + " wynosi " + obwod);
+      var obwod = 4 * wartosc_boku;
+      document.write("Obwód kwadratu o boku " + wartosc_boku + " wynosi " + obwod);
     }
 }
 
